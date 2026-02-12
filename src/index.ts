03c68f649f5d73952e6d9b7040e85c06de3777f8
Tienshiao Ma <tienshiao@tienshiao.org>
2026-02-12 09:43:30 -0800

    Add some React?

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
 import { Elysia } from "elysia";
+import { staticPlugin } from "@elysiajs/static";
+import { openapi, fromTypes } from "@elysiajs/openapi";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+import index from './index.html'
+
+export const app = new Elysia()
+	.use(
+		openapi({
+			references: fromTypes()
+		})
+	)
+	// .use(
+	// 	await staticPlugin({
+	// 		prefix: '/'
+	// 	})
+	// )
+  .get('/', index)
+	.get('/message', { message: 'Hello from server' } as const)
+	.listen(3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
